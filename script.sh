550c2304ba7672f04a29d598f6a65f8635959ffc
manishsemwal98 <87874409+manishsemwal98@users.noreply.github.com>
2022-04-27 14:32:10 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@
 
 if systemctl status docker.socket | grep "Active:" | grep -q "listening"
 then
-    echo -e "\e[32mSUCCESS:\e[0m Docker is running."
+    echo -e " Docker is running."
 else
     echo -e "Docker service is found in stopped state. \nStarting docker service......."
     sudo systemctl start docker.socket
